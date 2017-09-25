44770282cc37c6366889e974add6cf28cc3c1a86
Chad Crabtree <chad.rc.crabtree@gmail.com>
2017-09-25 19:35:41 -0400

    initial commit

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,46 @@
+package com.theironyard;
+
+/**
+ * Test Driven Development (TDD)
+ * Write your tests first and watch them fail.
+ * Then write the algorithm and watch the tests pass.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ReversePolishCalc rpc = new ReversePolishCalc();
+
+        String rpnExpression = "2.5,4.8,+";
+        double expectedResult = 7.3;
+        double actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "10,5,-";
+        expectedResult = 5;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "4,3.5,*";
+        expectedResult = 14;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "20,3.5,/";
+        expectedResult = 5.714285714285714;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "5,1,2,+,4,*,+,3,-";
+        expectedResult = 14.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+    }
+
+    private static void checkResult(String expression, double expected, double actual) {
+        if (expected == actual) {
+            System.out.println("SUCCESS: " + expression + " is " + expected);
+        } else {
+            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
+    }
+}
+
---
+++ src/com/theironyard/ReversePolishCalc.java
@@ -0,0 +1,57 @@
+package com.theironyard;
+
+public class ReversePolishCalc {
+
+    private int topOfStack = 0;
+
+    private String[] tokens;
+
+    private String[] stack;
+
+    public double calculate(String input) {
+
+        tokens = input.split(",");
+
+        stack = new String [tokens.length];
+
+        for(int i = 0; i < tokens.length; ++i) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                double number1 = pop();
+                double number2 = pop();
+                switch (tokens[i]) {
+                    case "+":
+                        push(number2 + number1);
+                        break;
+                    case "-":
+                        push(number2 - number1);
+                        break;
+                    case "*":
+                        push(number1 * number2);
+                        break;
+                    case "/":
+                        push(number2 / number1);
+                }
+            } else {
+                push(tokens[i]);
+            }
+        }
+        return pop();
+    }
+
+    private void push(String number) {
+        stack[topOfStack] = number;
+        topOfStack += 1;
+    }
+
+    private void push(double d) {
+        String temp = Double.toString(d);
+        stack[topOfStack] = temp;
+        topOfStack += 1;
+    }
+
+    private double pop() {
+        double result = Double.parseDouble(stack[topOfStack - 1]);
+        topOfStack -= 1;
+        return result;
+    }
+}
